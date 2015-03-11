2eefada1754225da36579c8a2d0f392169881a9d
GlassSources <luaswat@lukkit.gq>
2015-03-10 21:12:27 -0400

    updated shell to latest
    latest development version of the shell

---
+++ shell.lua
@@ -0,0 +1,26 @@
+local function announceTermMonitor(msgT, msgM, scale)
+    term.clear()
+    term.setCursorPos(1,1)
+    term.write(msgT)
+    term.setCursorPos(1,3)
+    for i=1, #positions do
+        if peripheral.getType(positions[i]) == "monitor" then
+            local mon = peripheral.wrap(positions[i])
+            if mon then
+                mon.setTextScale(scale)
+                mon.clear()
+                mon.setCursorPos(1,1)
+                mon.write(msgM)
+                mon.setCursorPos(1,3)
+            end
+        end
+    end
+end
+
+
+announceTermMonitor("Glass Console", "", 1)
+term.setCursorPos(1,2)
+term.write("Shell 2015.1.01.a/turtle Alpha")
+term.setCursorPos(1,4)
+sleep(0.1)
+shell.run("shell")
